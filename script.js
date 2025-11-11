5b08afa28558986f2a11713c0e44d4838fe73b5f
AhmedAbdulla22 <ahmeddev452@example.com>
2025-11-11 19:55:32 +0000

    Refactor gameBoard and Cell, win logic

--- script.js
+++ script.js
@@ -3,130 +3,61 @@ console.log("Welcome to tic-tac-toe!");
 
 // game board
 function gameBoard() {
-    let board = getBoard();
+    let board = newBoard();
 
+    function newBoard() {
+        let Board = [];
+        for (let i = 0; i < 9; i++) {
+            Board[i] = Cell();
+        } 
+        return Board;
+    }
+    
     
     function getBoard() {
-        return ["","","","","","","","",""];
+        return board;
     }
 
-    function resetBoard() {
-        board = getBoard();
+    function reset() {
+        board = newBoard();
     }
 
-    function TheBoardInterface() {
-        let boardUI = "";
-        for (let i = 0; i < 9; i++) {
-            boardUI += ` | ${board[i].getValue()}`;    
-            if (i === 2 || i === 4 || i === 8) {
-                boardUI += " |\n";
-            }
-        }
-        return boardUI;
+    // function TheBoardInterface() {
+    //     let boardUI = "";
+    //     for (let i = 0; i < 9; i++) {
+    //         boardUI += ` | ${board[i] ? board[i] : '#'}`;    
+    //         if (i === 2 || i === 5 || i === 8) {
+    //             boardUI += " |\n-----------\n";
+    //         }
+    //     }
+    //     return boardUI;
+    // }
+
+    function setMark(index,value) {
+        return board[index].setValue(value);
     }
 
-    function checkResult() {
-        //Horiziontal Check
-        for (let row = 0; row < 3; row++) {
-            //get first col value in the row 
-            let Symbol = board[row][0]?.getValue();
-            let match = true;
-
-            //compare first col value we got (Symbol)
-            for (let col = 0; col < 3; col++) {
-                if (board[row][col]?.getValue() !== Symbol || board[row][col]?.getValue() === "*") {
-                    match = false;
-                    break;
-                } 
-            }         
-
-            //if match then someone won
-            if (match) {
-                return true; //true = Won
-            }
-        }
-
-        //Vertical Check
-        for (let col = 0; col < 3; col++) {
-            //get first row value in the col
-            let Symbol = board[0][col]?.getValue();
-            let match = true;
-
-            //compare first row value we got (Symbol)
-            for (let row = 0; row < 3; row++) {
-                if (board[row][col]?.getValue() !== Symbol || board[row][col]?.getValue() === "*") {
-                    match = false;
-                    break;
-                } 
-            }         
-
-            //if match then someone won
-            if (match) {
-                return true;//true: Won
-            }
-        }
-
-        //Diagonal LTR Check
-        if (notMatch) {
-            //get first value from left to right
-            let Symbol = board[0][0]?.getValue();
-            let match = true;
-
-            for (let i = 0; i < 3; i++) {
-                if (board[i][i]?.getValue() !== Symbol || board[i][i]?.getValue() === "*") {
-                    match = false;
-                    break;
-                } 
-            }         
-
-            //if match then someone won
-            if (match) {
-                return true;//true: Won
-            }
-        } else {
-            //Diagonal RTL Check
-            //get first value from left to right
-            let Symbol = board[0][2]?.getValue();
-            let match = true;
-
-            for (let i = 0; i < 3; i++) {
-                //2-i for RTL
-                if (board[i][2 - i]?.getValue() !== Symbol || board[i][2 - i]?.getValue() === "*") {
-                    match = false;
-                    break;
-                } 
-            }         
-
-            //if match then someone won
-            if (match) {
-                return true;//true: Won
-            }
-        }
-
-        return false; //Not Won
-
-    }
-
-    function setCell(index,value) {
-        board[index].setValue(value);
-    }
-
-    return { TheBoardInterface,resetBoard , setCell,checkResult};
+    return { getBoard , reset , setMark};
 }
 
 // cell
 function Cell() {
-    //0 for empty
-    //1 for X
-    //2 for Y
-    let value = 0;
+    let value = "";
     function getValue() {
-        return value === 0 ? "*":
-          value === 1 ? "X":"O";
+        return value;
     }
 
     function setValue(newValue) {
-        value = newValue;
+        if(isEmpty()) {
+            value = newValue;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isEmpty() {
+        return value === "";
     }
 
     return {
@@ -148,49 +79,25 @@ function gameController() {
     let gameOver;
     let player = [];
     let currentPlayerIndex;
-    console.log(board);
+    let rounds;
 
     function play() {
-        board = ["","","","","","","","",""];
+        board = gameBoard();
         gameOver = false;
         player = [createPlayer("John","X"),createPlayer("Pork","O")];
         currentPlayerIndex = 0;
 
         while(!gameOver) {
-            let boardUI = "";
-            for (let i = 0; i < 9; i++) {
-                boardUI += ` | ${board[i] ? board[i] : i + 1}`;    
-                if (i === 2 || i === 5 || i === 8) {
-                    boardUI += " |\n-----------\n";
-                }
-            }
-
             const cell = prompt(`choose cell 1-9:\n${boardUI}`);
 
-            if(cell < 1 || cell > 9) {
-                alert("Wrong Choice!");
+            //drop value
+            if (!Board.setMark(cell,player[currentPlayerIndex].mark)) {
                 continue;
             }
             
-            //drop value
-            board[cell - 1] = player[currentPlayerIndex].mark;
 
             //check result
-            if (
-                //Horizontal 
-                board[0] === board[1] && board[1] === board[2] || 
-                board[3] === board[4] && board[4] === board[5] ||
-                board[6] === board[7] && board[7] === board[8] ||
-                //Vertical
-                board[0] === board[3] && board[3] === board[6] || 
-                board[1] === board[4] && board[4] === board[7] ||
-                board[2] === board[5] && board[5] === board[8] ||
-                //Diag
-                board[0] === board[4] && board[4] === board[8] ||
-                board[2] === board[4] && board[4] === board[6]
-            ) {
-                gameOver = true;
-            }
+           
 
             if (gameOver) {
                   console.log(`${player[currentPlayerIndex].name} Won the Game!`);
@@ -198,20 +105,81 @@ function gameController() {
 
             currentPlayerIndex = currentPlayerIndex === 0 ? 1:0;
         }
-
-
-
-        
-        //check who is turn
-        //wait for him to choose his cell
-        //drop value in that cell
-        //update board
-        // check who won
-        //if game over
-        // update player stats and game stats
-        // if not game over repeat to first step
     }
     
+    function checkWin(board) {
+        let isWin = false;
+        //Horizontal 
+            for (let row = 0; row < 3; row++) {
+                let index = row * 3; // to go by row each time 0 row 1 , 3 row 2 and 6 is row 3
+                let skip = false; // to skip row if there is empty cell
+
+                for (let i = 0; i < 3; i++) {
+                    if (board[index + i] === "") {
+                        skip = true;
+                        break;
+                    }                 
+                }
+
+                if(skip) {
+                   continue; 
+                } 
+
+                //check for win
+                if (board[index] === board[index + 1] && board[index] === board[index + 2]) {
+                    isWin = true;
+                    break;
+                }
+            }
+            
+            if (!isWin) {
+                //Vertical 
+                for (let col = 0; col < 3; col++) {
+                    let skip  = false; // to skip col if there is empty cell
+    
+                    for (let row = 0; row < 3; row++) {
+                        if (board[col + row * 3] === "") {
+                            skip = true;
+                            break;
+                        }                             
+                    }
+    
+                    if(skip) {
+                       continue; 
+                    } 
+    
+                    if (board[col] === board[col + 3] && board[col] === board[col + 6]) {
+                        isWin = true;
+                        break;
+                    }
+                }
+            }
+            
+
+            if (!isWin) {
+                //Diag 
+                alert("i'm here")
+                if (board[0] !== "" && board[0] === board[4] && board[0] === board[8]) {
+                    isWin = true;
+                } else if (board[2] !== "" && board[2] === board[4] && board[2] === board[6]) {
+                    isWin = true;
+                }
+            }
+
+            return isWin;
+    }
+
+    function checkDraw(board) {
+        let draw = true;
+        for (let i = 0; i < 9; i++) {
+            if (board[i] === "") {
+                draw = false;
+                break;
+            }
+        } 
+
+        return draw;
+    }
 
     return {board , play};
 }
